07937399c04a31a559c6a02d5fa37ae8efaa3656
Administrator <418556485@qq.com>
2024-10-20 19:17:25 +0800

    写法提交

--- common/concurrent/set.go
+++ common/concurrent/set.go
@@ -6,58 +6,57 @@ import (
 
 // ConcurrentSet 是一个线程安全的集合，可以添加和删除元素。
 type ConcurrentSet[T comparable] struct {
-	lock  sync.RWMutex
+	sync.RWMutex
 	items map[T]struct{}
 }
 
 // NewConcurrentSet 创建一个新的 ConcurrentSet 实例。
 func NewConcurrentSet[T comparable]() *ConcurrentSet[T] {
 	return &ConcurrentSet[T]{
-		lock:  sync.RWMutex{},
 		items: make(map[T]struct{}),
 	}
 }
 
 // Add 向集合中添加一个元素。
 func (set *ConcurrentSet[T]) Add(item T) {
-	set.lock.Lock()
-	defer set.lock.Unlock()
+	set.Lock()
+	defer set.Unlock()
 	set.items[item] = struct{}{}
 }
 
 // Remove 从集合中移除一个元素。
 func (set *ConcurrentSet[T]) Remove(item T) {
-	set.lock.Lock()
-	defer set.lock.Unlock()
+	set.Lock()
+	defer set.Unlock()
 	delete(set.items, item)
 }
 
 // Has 检查集合中是否存在一个元素。
 func (set *ConcurrentSet[T]) Has(item T) bool {
-	set.lock.RLock()
-	defer set.lock.RUnlock()
+	set.RLock()
+	defer set.RUnlock()
 	_, exists := set.items[item]
 	return exists
 }
 
 // Clear 清空集合中的所有元素。
 func (set *ConcurrentSet[T]) Clear() {
-	set.lock.Lock()
-	defer set.lock.Unlock()
+	set.Lock()
+	defer set.Unlock()
 	set.items = make(map[T]struct{})
 }
 
 // Size 返回集合中元素的数量。
 func (set *ConcurrentSet[T]) Size() int {
-	set.lock.RLock()
-	defer set.lock.RUnlock()
+	set.RLock()
+	defer set.RUnlock()
 	return len(set.items)
 }
 
 // All 返回一个包含集合中所有元素的切片。
 func (set *ConcurrentSet[T]) All() []T {
-	set.lock.RLock()
-	defer set.lock.RUnlock()
+	set.RLock()
+	defer set.RUnlock()
 	// 获取所有键
 	var keys []T
 	for key := range set.items {
